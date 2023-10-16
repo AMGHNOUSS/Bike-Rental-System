292668c446ca6425b22b09843450dd5d9b8cb82a
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-16 23:18:28 +0100

    add comment

--- Bike-Rental-System-OOP/BikeRental.py
+++ Bike-Rental-System-OOP/BikeRental.py
@@ -9,4 +9,5 @@ class BikeRental():
         self.stock = 100
 
     def displaystock(self):
+        """Display content of bikes"""
         print(f"We have currently {self.stock} bikes available to rent.")
