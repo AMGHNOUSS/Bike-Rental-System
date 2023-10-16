8f0965203a1e04da5cab8c5b7e916d9ae4e93c47
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-16 22:45:16 +0100

    Create File main.py

---
+++ Bike-Rental-System-OOP/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# The main program.
+if __name__ == "__main__":
+    while True:
+        print("1- See available bikes on the shop")
+        print("2- Rent bikes on hourly basis $5 per hour.")
+        print("3- Rent bikes on daily basis $20 per day.")
+        print("4- Rent bikes on weekly basis $60 per week.")
+        print("5- Family Rental, a promotion that can include from 3 to 5 Rentals (of any type) with a discount of 30% of the total price")
+        print("6- Exit")
+        print()
+
+        result = int(input("Your Choice:"))
+        if result == 1:
+            pass
+        elif result == 2:
+            pass
+        elif result == 3:
+            pass
+        elif result == 4:
+            pass
+        elif result == 5:
+            pass
+        elif result == 6:
+            break
