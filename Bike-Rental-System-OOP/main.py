d2ac90ab7780f583d496297b25c6e25aefb48f63
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-18 17:01:16 +0100

    Fix main

--- Bike-Rental-System-OOP/main.py
+++ Bike-Rental-System-OOP/main.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
 from BikeRental import BikeRental
+from Customer import Customer
 # The main program.
 if __name__ == "__main__":
     rb = BikeRental()
+    cus = Customer()
+    listCustomer = []
     while True:
-        print("1- See available bikes on the shop")
-        print("2- Rent bikes on hourly basis $5 per hour.")
-        print("3- Rent bikes on daily basis $20 per day.")
-        print("4- Rent bikes on weekly basis $60 per week.")
-        print("5- Family Rental, a promotion that can include from 3 to 5 Rentals (of any type) with a discount of 30% of the total price")
-        print("6- Exit")
+        print("\t=========== Bike rental shop ================")
+        print("\t1- See available bikes on the shop")
+        print("\t2- Rent bikes on hourly basis $5 per hour.")
+        print("\t3- Rent bikes on daily basis $20 per day.")
+        print("\t4- Rent bikes on weekly basis $60 per week.")
+        print("\t5- Family Rental, a promotion that can include from 3 to 5 Rentals (of any type) with a discount of 30% of the total price")
+        print("\t6- Exit")
+        print("\t===============================================")
         print()
 
         choix = int(input("Your Choice:"))
         if choix == 1:
             rb.displaystock()
         elif choix == 2:
-            dateRental = rb.rentBikeOnHourly(2)
+            customer = cus.requestBike(choix)
+            listCustomer.append(customer)
+            rb.rentBikeOnHourly(customer['bikes'])
         elif choix == 3:
-            dateRental = rb.rentBikeOnDaily(3)
+            customer = cus.requestBike(choix)
+            listCustomer.append(customer)
+            rb.rentBikeOnDaily(customer['bikes'])
         elif choix == 4:
-            dateRental = rb.rentBikeOnDaily(1)
+            customer = cus.requestBike(choix)
+            listCustomer.append(customer)
+            rb.rentBikeOnWeekly(customer['bikes'])
         elif choix == 5:
-            rb.returnBike(3, choix, dateRental)
+            pass
         elif choix == 6:
             break
