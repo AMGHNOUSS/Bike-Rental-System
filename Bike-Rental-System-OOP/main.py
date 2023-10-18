8f59226b8bb82d49727d690a5edac3bc8bea249e
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-18 21:57:37 +0100

    Fix Erroe

--- Bike-Rental-System-OOP/main.py
+++ Bike-Rental-System-OOP/main.py
@@ -11,15 +11,14 @@ if __name__ == "__main__":
     listCustomer = []
     while True:
         print()
-        print("\t ====================== Bike rental shop ============================")
-        print("\t| 1- See available bikes on the shop                                 |")
-        print("\t| 2- Rent bikes on hourly basis $5 per hour.                         |")
-        print("\t| 3- Rent bikes on daily basis $20 per day.                          |")
-        print("\t| 4- Rent bikes on weekly basis $60 per week.                        |")
-        print("\t| 5- Family Rental, a promotion that can includefrom 3 to 5 Rentals  |") 
-        print("\t|   (of any type) with a discount of 30% of the total price          |")
-        print("\t| 6- Exit                                                            |")
-        print("\t ====================================================================")
+        print("\t ============= Bike rental shop ==============")
+        print("\t| 1- See available bikes on the shop          |")
+        print("\t| 2- Rent bikes on hourly basis $5 per hour.  |")
+        print("\t| 3- Rent bikes on daily basis $20 per day.   |")
+        print("\t| 4- Rent bikes on weekly basis $60 per week. |")
+        print("\t| 5- Return a bike.                           |") 
+        print("\t| 6- Exit.                                    |")
+        print("\t =============================================")
         print()
 
         choix = int(input("Your Choice: "))
@@ -38,10 +37,14 @@ if __name__ == "__main__":
             listCustomer.append(customer)
             rb.rentBikeOnWeekly(customer['bikes'], customer['rentalTime'])
         elif choix == 5:
+            print(listCustomer)
             rem = cus.returnBike(listCustomer)
             listCustomer.pop(rem[0])
             rb.returnBike(rem[1], rem[2], rem[3])
         elif choix == 6:
             break
-        time.sleep(10)
+        else:
+            print("Number incorrect")
+            print()
+        input("Tape eny button.")
         os.system('cls' if os.name == 'nt' else 'clear')
