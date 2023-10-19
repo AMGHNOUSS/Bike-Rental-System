5a9157d0fa859f11b185c53e90824a4860909f2b
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-19 22:37:03 +0100

    Create Function test_displaystock()

--- Bike-Rental-System-OOP/test/BikeRentalTest.py
+++ Bike-Rental-System-OOP/test/BikeRentalTest.py
@@ -5,3 +5,10 @@ import unittest
 
 class BikeRentalTest():
     """Class Test For class BikeRental"""
+
+    def test_displaystock(self):
+        """Like 100 bikes"""
+        shop1 = BikeRental()
+        shop2 = BikeRental(10)
+        self.assertEqual(shop1.displaystock(), 0)
+        self.assertEqual(shop2.displaystock(), 10)
