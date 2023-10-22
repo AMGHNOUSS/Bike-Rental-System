85a60d8e5eb4c42e46620d9a783cfd93c169b225
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-22 21:15:35 +0100

    Test for rentBikeOnHourly

--- Bike-Rental-System-OOP/test/BikeRentalTest.py
+++ Bike-Rental-System-OOP/test/BikeRentalTest.py
@@ -1,14 +1,43 @@
 #!/usr/bin/python3
 
-from BikeRental import BikeRental
+from  BikeRental import BikeRental
 import unittest
+import datetime
 
-class BikeRentalTest():
+class BikeRentalTest(unittest.TestCase):
     """Class Test For class BikeRental"""
 
     def test_displaystock(self):
-        """Like 100 bikes"""
-        shop1 = BikeRental()
-        shop2 = BikeRental(10)
-        self.assertEqual(shop1.displaystock(), 0)
-        self.assertEqual(shop2.displaystock(), 10)
+        """100 bikes"""
+        bk = BikeRental()
+        self.assertEqual(bk.displaystock(), 100)
+    
+    def test_rentBikeOnHourly(self):
+        """Rent some bikes on hourly basis"""
+        bk = BikeRental()
+        dt = datetime.datetime.now()
+        self.assertEqual(bk.rentBikeOnHourlyBasis(-1, dt), False)
+    
+    def test_rentBikeOnHourly_for_number_negative(self):
+        """Rent some bikes on hourly basis"""
+        bk = BikeRental()
+        dt = datetime.datetime.now()
+        self.assertEqual(bk.rentBikeOnHourlyBasis(-1, dt), False)
+    
+    def test_rentBikeOnHourly_for_zero(self):
+        """Rent some bikes on hourly basis"""
+        bk = BikeRental()
+        dt = datetime.datetime.now()
+        self.assertEqual(bk.rentBikeOnHourlyBasis(0, dt), False)
+    
+    def test_rentBikeOnHourly_for_number_positive(self):
+        """Rent some bikes on hourly basis"""
+        bk = BikeRental()
+        dt = datetime.datetime.now()
+        self.assertEqual(bk.rentBikeOnHourlyBasis(2, dt), True)
+
+    def test_rentBikeOnHourly_for_number_positive_big_than_bike(self):
+        """Rent some bikes on hourly basis"""
+        bk = BikeRental()
+        dt = datetime.datetime.now()
+        self.assertEqual(bk.rentBikeOnHourlyBasis(101, dt), False)
