0f40288c1fc7a6b19ce0a2387e153a07e032ef13
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-25 23:10:14 +0100

    Fix function testing

--- Bike-Rental-System-OOP/CustomerTest.py
+++ Bike-Rental-System-OOP/CustomerTest.py
@@ -10,4 +10,12 @@ class CustomerTest(unittest.TestCase):
 
     def setUp(self):
         """initialize Object"""
-        cus = Customer()
+        self.cus = Customer()
+    
+    def test_requestBike_for_bike_neg(self):
+        """test for requestBike nagative"""
+        self.assertEqual(self.cus.requestBike(2, -3), None)
+    
+    def test_requestBike_for_bike_pos(self):
+        """test for requestBike positive"""
+        self.assertEqual(type(self.cus.requestBike(2, 2)), dict)
